77f73f2919becd352511f3ae7a8db3c0e55b8f53
Arthur Dubedat <arthur.dubedat@gmail.com>
2019-04-17 16:41:12 +0200

    ft_toupper, ft_tolower done

--- src/ft_tolower.s
+++ src/ft_tolower.s
@@ -2,4 +2,16 @@ section .text
     global _ft_tolower
 
 _ft_tolower:
+    push rbp                ; save stack
+    mov rbp, rsp            ; clean stack
+    mov rax, rdi
+    sub rdi, 65             ; substract 'A' from the tested value, setting the 0 to 'A'
+    cmp rdi, 25             ; unsigned comparison allowing to check a range with only one comparison
+    ja .end                 ; if not a lowercase, jump directly to end, else, remove 32 to transform into an lowercase
+
+.success:
+    add rax, 32
+
+.end:
+    leave
     ret
--- src/ft_toupper.s
+++ src/ft_toupper.s
@@ -2,4 +2,16 @@ section .text
     global _ft_toupper
 
 _ft_toupper:
+    push rbp                ; save stack
+    mov rbp, rsp            ; clean stack
+    mov rax, rdi
+    sub rdi, 97             ; substract 'a' from the tested value, setting the 0 to 'a'
+    cmp rdi, 25             ; unsigned comparison allowing to check a range with only one comparison
+    ja .end                 ; if not a lowercase, jump directly to end, else, remove 32 to transform into an uppercase
+
+.success:
+    sub rax, 32
+
+.end:
+    leave
     ret
