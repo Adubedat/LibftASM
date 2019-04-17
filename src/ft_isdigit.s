478da223edfd3d29f48b1abdca8b883c4cec2b5c
Arthur Dubedat <arthur.dubedat@gmail.com>
2019-04-17 16:02:41 +0200

    ft_isdigit done

--- src/ft_isdigit.s
+++ src/ft_isdigit.s
@@ -1,5 +1,20 @@
+%define TRUE        1
+%define FALSE       0
+
 section .text
     global _ft_isdigit
 
 _ft_isdigit:
+    push rbp                ; save stack
+    mov rbp, rsp            ; clean stack
+    sub rdi, 48             ; substract '0' from the tested value, setting the 0 to ascii '0'
+    cmp rdi, 9              ; unsigned comparison allowing to check a range with only one comparison
+    jbe .success            
+    mov rax, FALSE
+    leave
+    ret
+
+.success:
+    mov rax, TRUE
+    leave
     ret
